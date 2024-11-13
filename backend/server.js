652d6c12699a5ddf6ff906eb371466f46f2101fb
Marcy Thany <marcythany@gmail.com>
2024-11-13 11:08:59 -0300

    feat: adição de cors e rotas de busca

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import { connectDB } from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import videoRoutes from './routes/videoRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
+import searchRoutes from './routes/searchRoutes.js';
+import cors from 'cors';
 
 // Configura as variáveis de ambiente
 dotenv.config();
@@ -17,10 +19,19 @@ const PORT = process.env.PORT || 5000;
 // Middleware para parsing de JSON
 app.use(express.json());
 
+// Configuração do CORS
+app.use(
+	cors({
+		origin: 'http://localhost:3000', // Permite requisições do frontend
+		methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP necessários
+	})
+);
+
 // Rotas
 app.use('/api/auth', authRoutes);
 app.use('/api/videos', videoRoutes);
 app.use('/api/profile', profileRoutes);
+app.use('/api', searchRoutes);
 
 // Início do servidor
 app.listen(PORT, () => {
