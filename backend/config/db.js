2a4b6343ea5527189e9f2c6b9130e221a41b718a
Marcy Thany <marcythany@gmail.com>
2024-11-12 21:38:39 -0300

    feat: conexão com banco de dados estabelecida!

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,11 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const connectDB = async () => {
+export const connectDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log('MongoDB conectado');
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		process.exit(1); // process code 1 code means exit with failure, 0 means success
 	}
 };
-
-module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-const videoRoutes = require('./routes/videoRoutes');
+import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/videos', videoRoutes);
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log('Conectado ao MongoDB');
-		app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-	})
-	.catch((err) => console.error(err));
+app.listen(PORT, () => {
+	connectDB();
+	console.log('Server started at http://localhost:' + PORT);
+});
