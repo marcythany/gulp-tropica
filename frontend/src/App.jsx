c2883e33762461bb80f024dab250bb080865df6d
Marcy Thany <marcythany@gmail.com>
2024-11-13 08:30:34 -0300

    feat: ajuste no caminho para maior segurança

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,43 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
 import FavoritesPage from './pages/FavoritesPage';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/profile' element={<ProfilePage />} />
-				<Route path='/favorites' element={<FavoritesPage />} />
-			</Routes>
-		</Router>
+		<AuthProvider>
+			<Router>
+				<div className='min-h-screen bg-gray-100'>
+					<Navbar />
+					<main className='container mx-auto py-6 px-4'>
+						<Routes>
+							<Route path='/' element={<HomePage />} />
+							<Route path='/login' element={<LoginPage />} />
+							<Route
+								path='/profile'
+								element={
+									<PrivateRoute>
+										<ProfilePage />
+									</PrivateRoute>
+								}
+							/>
+							<Route
+								path='/favorites'
+								element={
+									<PrivateRoute>
+										<FavoritesPage />
+									</PrivateRoute>
+								}
+							/>
+						</Routes>
+					</main>
+				</div>
+			</Router>
+		</AuthProvider>
 	);
 };
 
