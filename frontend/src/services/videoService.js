7f2891817e33cabb48a716fa55d89f8b39d455c8
Marcy Thany <marcythany@gmail.com>
2024-11-13 11:57:39 -0300

    feat: adição funções busca videos e remove dos favoritos

--- frontend/src/services/videoService.js
+++ frontend/src/services/videoService.js
@@ -1,6 +1,7 @@
+// videoService.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/videos'; // Substitua pelo URL da sua API
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Função para buscar vídeos com base em tags
 export const fetchVideos = async (tag) => {
@@ -14,21 +15,37 @@ export const fetchVideos = async (tag) => {
 	}
 };
 
+// Função para buscar vídeos com base em tags (adicionada como 'searchVideos')
+export const searchVideos = async (tag) => {
+	return fetchVideos(tag); // Reutilizando a função fetchVideos
+};
+
 // Função para adicionar vídeo aos favoritos
-export const addFavorite = async (videoId) => {
+export const addToFavorites = async (videoData) => {
 	try {
 		const token = localStorage.getItem('userToken');
-		const response = await axios.post(
-			`${API_URL}/favorites`,
-			{ videoId },
-			{
-				headers: { Authorization: `Bearer ${token}` },
-			}
-		);
+		const response = await axios.post(`${API_URL}/favorites`, videoData, {
+			headers: { Authorization: `Bearer ${token}` },
+		});
 		return response.data;
 	} catch (error) {
 		throw new Error(
 			error.response.data.message || 'Erro ao adicionar aos favoritos'
+		);
+	}
+};
+
+// Função para remover vídeo dos favoritos
+export const removeFromFavorites = async (videoId) => {
+	try {
+		const token = localStorage.getItem('userToken');
+		const response = await axios.delete(`${API_URL}/favorites/${videoId}`, {
+			headers: { Authorization: `Bearer ${token}` },
+		});
+		return response.data;
+	} catch (error) {
+		throw new Error(
+			error.response.data.message || 'Erro ao remover dos favoritos'
 		);
 	}
 };
