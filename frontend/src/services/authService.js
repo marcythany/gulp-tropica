c042e9c0aedb3cf88501eb2a7b9282d23d7fd06d
Marcy Thany <marcythany@gmail.com>
2024-11-13 11:56:39 -0300

    feat: adição de função para atualizar perfil

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth'; // Substitua pelo URL da sua API
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Função para registro de novo usuário
 export const register = async (userData) => {
@@ -29,9 +29,31 @@ export const login = async (userData) => {
 // Função para logout do usuário
 export const logout = () => {
 	localStorage.removeItem('userToken');
+	localStorage.removeItem('user');
 };
 
 // Função para obter o token do usuário autenticado
 export const getToken = () => {
 	return localStorage.getItem('userToken');
+};
+
+// Função para atualizar o perfil do usuário
+export const updateProfile = async (userData) => {
+	try {
+		const token = getToken();
+		const response = await axios.put(`${API_URL}/profile`, userData, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		return response.data;
+	} catch (error) {
+		throw new Error(error.response.data.message || 'Erro ao atualizar perfil');
+	}
+};
+
+// Função para obter dados do usuário logado
+export const getUser = () => {
+	const user = localStorage.getItem('user');
+	return user ? JSON.parse(user) : null;
 };
