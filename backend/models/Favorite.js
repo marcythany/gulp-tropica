a4b13cd9cf93f9df1ea9b19c5c93d01accc15bde
Marcy Thany <marcythany@gmail.com>
2024-11-13 11:00:28 -0300

    fix: garatir que não haja erro quando favoritar videos.

--- backend/models/Favorite.js
+++ backend/models/Favorite.js
@@ -5,11 +5,18 @@ const FavoriteSchema = new mongoose.Schema(
 		userId: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'User',
-			required: true,
+			required: [true, 'O campo userId é obrigatório'],
 		},
-		videoId: { type: String, required: true },
-		title: { type: String, required: true },
-		// Adicione outros campos conforme necessário
+		videoId: {
+			type: String,
+			required: [true, 'O campo videoId é obrigatório'],
+			trim: true, // Garantir que não haja espaços extras no início ou fim
+		},
+		title: {
+			type: String,
+			required: [true, 'O campo title é obrigatório'],
+			trim: true, // Garantir que o título não tenha espaços extras
+		},
 	},
 	{ timestamps: true }
 );
